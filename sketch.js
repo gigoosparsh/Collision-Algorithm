b6d0a484728ca8cfb87fafadc7c566aff74b4d39
Lucifer Dark <75560092+gigoosparsh@users.noreply.github.com>
2020-12-15 18:01:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var movingrect,fixedrect;
+
+function setup() {
+  createCanvas(800,400);
+  //createSprite(400, 200, 50, 50)
+  movingrect=createSprite(400, 200, 80, 50)
+  movingrect.shapeColor="green";
+  movingrect.debug=true;
+  fixedrect=createSprite(400, 200, 50, 50)
+  fixedrect.shapeColor="green";
+  fixedrect.debug=true;
+  
+
+}
+
+function draw() {
+  background(255,255,255);
+  movingrect.x=World.mouseX;
+  movingrect.y=World.mouseY;  
+  console.log(movingrect.x-fixedrect.x);
+  if(  movingrect.x - fixedrect.x < fixedrect.width/2 + movingrect.width/2
+    && fixedrect.x - movingrect.x < fixedrect.width/2 + movingrect.width/2
+    && movingrect.y - fixedrect.y < fixedrect.width/2 + movingrect.width/2
+    && fixedrect.y - movingrect.y < fixedrect.width/2 + movingrect.width/2)
+    {
+    movingrect.shapeColor="red";  
+    fixedrect.shapeColor="red"; 
+  }
+  else{
+    movingrect.shapeColor="green";
+    fixedrect.shapeColor="green";
+  }
+  
+  drawSprites();
+}
